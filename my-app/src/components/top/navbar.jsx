f7a309021ba8b1e42ba357cc8d9b2f7a3341ff63
clopez11224 <christopherlopez000@gmail.com>
2022-11-30 14:35:00 -0500

    Setup routes

---
+++ my-app/src/components/top/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar () {
+    return (
+        <div>
+            <nav>
+                Hello World
+            </nav>
+        </div>
+
+    )
+}
+
