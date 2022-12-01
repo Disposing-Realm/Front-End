6c423367579c12ccf2c1c862d1da791216c23668
clopez11224 <christopherlopez000@gmail.com>
2022-12-01 13:42:43 -0500

    Quick styling of homepage

--- my-app/src/pages/home/home.jsx
+++ my-app/src/pages/home/home.jsx
@@ -1,17 +1,22 @@
 import NavBar from "../../components/top/navbar.jsx"
 import Hometext from "../../components/hometext/hometext.jsx"
+import logo from '../../logo.svg';
+import "./home.css"
 
 export default function Home(/*{setAuth}*/) {
-    // const { user } = useContext(AppContext)
-    return (
-      <>
-        <NavBar /*key={user.user_id} userInfo={user} setAuth={setAuth}*//>
-        <Hometext id="hometext"/>
-        {/* <div className="homeContainer">
+  // const { user } = useContext(AppContext)
+  return (
+    <>
+      <NavBar /*key={user.user_id} userInfo={user} setAuth={setAuth}*/ />
+      <div id="first-section">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Hometext id="hometext" />
+      </div>
+      {/* <div className="homeContainer">
           <Sidebar />
           <Feed key={user.user_id} userInfo={user}/>
           <Rightbar/>
         </div> */}
-      </>
-    )
-  }
+    </>
+  )
+}
