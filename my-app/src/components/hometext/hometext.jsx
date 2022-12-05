42abf5ecc85792a6a40a0097a046f4fba4e5433c
RichardCastillo1226 <castillo.richard1226@gmail.com>
2022-12-05 01:52:58 -0500

    added info

--- my-app/src/components/hometext/hometext.jsx
+++ my-app/src/components/hometext/hometext.jsx
@@ -4,14 +4,8 @@ import "./hometext.css"
 export default function Hometext () {
     return (
         <div id="text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in metus non metus pretium mattis quis tincidunt tellus.
-                 Morbi lobortis tincidunt quam non rutrum. Sed et fringilla neque, eu vestibulum lorem. Ut ut tellus magna. Integer ut 
-                 efficitur neque. Etiam vestibulum est sit amet enim pellentesque, porttitor faucibus nisl ultrices. Suspendisse sodales 
-                 luctus velit, a pharetra leo commodo ut. Aliquam nec metus non magna fermentum rutrum vitae interdum magna. Mauris ut 
-                 ultricies nulla, sed finibus dui. Maecenas vestibulum, ex nec porta elementum, elit nulla vehicula neque, quis tempus
-                  arcu dui sit amet sem. Pellentesque rhoncus aliquam dui, in posuere leo vulputate sed. Integer varius libero urna, 
-                  eget congue ligula rutrum non. Quisque eu massa tempus eros pulvinar elementum porttitor ut ante. Suspendisse scelerisque
-                   lacinia diam in finibus.</p>
+            
         </div>
+        
     )
 }
