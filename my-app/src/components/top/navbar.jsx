eefb671cdfbdf3a8ddc3281c283109a324554759
evanlu1134 <evanlu1134@gmail.com>
2022-12-12 21:42:28 -0500

    feat:added my leaderboard and background implementations

--- my-app/src/components/top/navbar.jsx
+++ my-app/src/components/top/navbar.jsx
@@ -3,82 +3,81 @@ import { UserContext } from "../../context/userContext";
 import { Link } from "react-router-dom";
 import { Nav } from "react-router-dom";
 import { redirect } from "react-router-dom";
-
-
-
+import Background from "../../background/background.js"
 import AppContext from "../../context/appContext";
 import "./navbar.css"
+import Leaderboard from "../../pages/leaderboard/leaderboard";
 
 export default function NavBar() {
-    // const { setIsAuth } = useContext(AppContext);
-    // const loggedIn = window.localStorage.getItem("isLoggedIn");
-    // const [anchorElNav, setAnchorElNav] = useState(null);
-    // const [anchorElUser, setAnchorElUser] = useState(null);
+  // const { setIsAuth } = useContext(AppContext);
+  // const loggedIn = window.localStorage.getItem("isLoggedIn");
+  // const [anchorElNav, setAnchorElNav] = useState(null);
+  // const [anchorElUser, setAnchorElUser] = useState(null);
 
-    return (
-        <>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-  {/* <figure class="image is-128x128">
-  <img src={require('./dp.png')}>
-</figure> */}
-    <a class="navbar-item">
-    <figure class="image is-10x10">
-  <img src={require('./dp.png')}/>
-</figure>
-      {/* <img src={require('./dp.png')}  alt = "love" width="169" height="169"/> */}
-    </a>
-
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
+  return (
+    <>
+  
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a>
+            <Link to="/">
+              <figure class="image is-96x96">
+                <img src={require('./dp.png')} />
+              </figure>
+            </Link>
           </a>
-          <a class="navbar-item">
-            Profile
-          </a>
-          <a class="navbar-item">
-            Leaderboard
-          </a>
-          <hr class="navbar-divider" />
-        </div>
-      </div>
-    </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
         </div>
-      </div>
-    </div>
-  </div>
-</nav>
-        </>
 
-    )
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Home
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
+                </a>
+                <a class="navbar-item">
+                  Profile
+                </a>
+                <Link to="/leaderboard">
+                  <div className="linkers"> LeaderBoard</div>
+                </Link>
+                <hr class="navbar-divider" />
+              </div>
+            </div>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Background>
+      </Background>
+    </>
+  )
 }
 
 
